a3fe8ab1713bb64a3424f7058b12d79a5648ebc6
phultquist <phultquist@imsa.edu>
2021-10-23 22:21:19 -0400

    all

--- admin/components/SideBar.js
+++ admin/components/SideBar.js
@@ -46,7 +46,7 @@ const SideBar = ({ currentPage }) => {
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                                         </svg>
                                     </SideBarItem>
-                                    <SideBarItem
+                                    {/* <SideBarItem
                                         name="Graphs"
                                         href="/graphs"
                                         selected={currentPage === "/graphs"}
@@ -54,7 +54,7 @@ const SideBar = ({ currentPage }) => {
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                         </svg>
-                                    </SideBarItem>
+                                    </SideBarItem> */}
                                     <SideBarItem
                                         name="Settings"
                                         href="/settings"
