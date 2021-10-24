731b9a3122835c7062c7a44a4cd0e6605b684eb6
phultquist <phultquist@imsa.edu>
2021-10-23 23:13:40 -0400

    changes

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -2,7 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import SideBar from "../components/SideBar";
 
-export default function Home() {
+export default function Home({ problemHistory }) {
+  const problemString = JSON.stringify(problemHistory);
+
   return (
     <>
       <Head>
@@ -11,8 +13,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className='text-3xl font-bold'>Good Days Cafe</h1>
-      <iframe src="/map/index.html?name=patrick" className='m-4 w-full h-4/5'></iframe>
+      <iframe src={`/map/index.html?data=${problemString}`} className='m-4 w-full h-4/5'></iframe>
     </>
 
   )
+}
+
+export async function getServerSideProps() {
+  const problemHistoryRes = await fetch(`http://localhost:5000/history`);
+  const problemHistory = await problemHistoryRes.json();
+
+  return {
+    props: {
+      problemHistory
+    }
+  }
 }
--- admin/pages/settings.js
+++ admin/pages/settings.js
@@ -1,8 +1,22 @@
-export default function Settings() {
-    return (
-        <div>
-            <h1 className='text-3xl font-bold'>Settings</h1>
+import { useState } from "react"
 
+export default function Settings() {
+    const [name, setName] = useState("Good Day Cafe");
+    return (
+        <div className="space-y-4">
+            <h1 className='text-3xl font-bold'>Settings</h1>
+            <div className="space-y-2">
+                <h2 className="text-xl font-semibold">Name</h2>
+                <input className="border-2 border-gray-200 p-2 w-1/3 rounded-md outline-none" value={name} onChange={(e) => {
+                    setName(e.target.value);
+                }}/>
+            </div>
+            <div className="space-y-2">
+                <h2 className="text-xl font-semibold">Profile Picture</h2>
+            </div>
+            <div className="space-y-2">
+                <h2 className="text-xl font-semibold">Banner Picture</h2>
+            </div>
         </div>
     )
 }
